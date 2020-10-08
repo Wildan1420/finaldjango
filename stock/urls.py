5f984e02d05d76d21200e3d358c3f4684a1cc211
Wildan1420 <61011420@kmitl.ac.th>
2020-10-08 15:38:41 +0700

    final project

--- stock/urls.py
+++ stock/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='base'),
-    path('index', views.index, name='index'),
+
+    path('', views.index, name='index'),
     path('About', views.About, name='About'),
-    path('Contact', views.Contact, name='Contact')
+    path('Contact', views.Contact, name='contact'),
 
 ]
